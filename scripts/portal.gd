26efba738ae1ed474bc46bcf54a08c03a2cf506e
Slime <amitbro640@gmail.com>
2026-09-19 14:14:08 +0545

    SLIME: UI? also portalsssss

---
+++ scripts/portal.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+@export var destination: Area2D
+var can_teleport = true
+
+func _on_body_entered(body):
+	if not can_teleport:
+		return
+		print("wrong can teleport")
+	
+	if body.is_in_group("player"):
+		print("the player is called")
+	if can_teleport:
+			can_teleport = false
+			destination.can_teleport = false
+			
+			body.global_position = destination.global_position
+			print(destination.global_position)
+			
+			await get_tree().create_timer(0.5).timeout
+			can_teleport = true
+			destination.can_teleport = true
+			print("should work?")
+			
